2dfa85e6b44f0e904d0571bb635dbc825d15579e
ClarkAltius <ilhwana@gmail.com>
2025-09-19 16:05:32 +0900

    day3 on spring boot

--- src/main/java/com/coffee/repository/MemberRepository.java
+++ src/main/java/com/coffee/repository/MemberRepository.java
@@ -7,5 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 //회원 정보들을 이용하여 데이터베이스와 교신하는 인터페이스
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-
+    //이메일 정보를 이용하여 해당 회원이 존재하는지 체크
+    //findByEmail은 JPA에서 쿼리 메소드 라고 칭한다
+    //이메일 정보를 이용해서 해당 회원이 존재하는 지 체크
+    Member findByEmail(String email);
 }
---
+++ src/main/java/com/coffee/service/MemberService.java
@@ -0,0 +1,27 @@
+package com.coffee.service;
+
+import com.coffee.constant.Role;
+import com.coffee.entity.Member;
+import com.coffee.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+@Service //서비스 역할을 하며, 주로 로직 처리에 활용되는 자바 클래스
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public Member findByEmail(String email) {
+        return memberRepository.findByEmail(email);
+    }
+    public void insert(Member bean){
+        //사용자 역할과 등록 일자는 여기서 입력
+        bean.setRole(Role.USER);
+        bean.setRegdate(LocalDate.now());
+        //주의) repository 에서 인서트 작업은 save메소드 사용
+
+    memberRepository.save(bean);
+    }
+}
